3615f17d25e01ecac1ac137ff820d7447a7a6fac
da_opanserh <da_opanserh@lan.itae.local>
2025-04-29 12:33:04 +0200

    optimizacion

--- useraddons/linea/controllers/lineasController.py
+++ useraddons/linea/controllers/lineasController.py
@@ -4,27 +4,32 @@ from odoo.http import request
 
 class lineasController(http.Controller):
     @http.route('/linea/dashboard', type='http', auth='public', website=True)
-    def dashboard_view(self):       
- 
-        hoy = date.today() # Fecha segun la zona horaria del ordenador 
-        dia_semana =  datetime.today().strftime('%A')  # Dia de la semana (lunes, martes,etc..) 
-        dia_hoy = fields.Date.today() # Fecha segun la zona horaria de odoo 
-        hora_actual = (datetime.now() + timedelta(hours=2)).strftime('%H:%M') # Ajustar la zona horaria a UTC+2
+    def dashboard_view(self):      
 
-        # Horas y puestos fijos
+        ######### Variables ######## 
+
+        # Fechas y hora actual
+        hoy = date.today() # Segun zona horaria del ordenador 
+        dia_semana =  datetime.today().strftime('%A') # Lunes, martes,etc...
+        dia_hoy = fields.Date.today() # Segun zona horaria de odoo 
+        hora_actual = (datetime.now() + timedelta(hours=2)).strftime('%H:%M') 
+
+        # Días, horas y puestos fijos
+        dias_orden = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes']
         horas = ['07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00'] 
         puestos = ['1', '2', '3', '4', '5']
+
+        # Inicio y fin de la semana actual
+        inicio_semana = hoy - timedelta(days=hoy.weekday())  
+        fin_semana = inicio_semana + timedelta(days=4) 
+
+        ######### Gestionar linea ########
 
         # Obtener los registros de la tabla 'linea' para el día actual
         registros_linea_hoy = request.env['linea'].sudo().search([('timestamp', '>=', dia_hoy), ('timestamp', '<', fields.Date.add(dia_hoy, days=1))])
 
-        # Diccionario de registros por hora y puesto
-        # {('07:00', '3'): 1, ('11:00', '3'): 2, ('11:00', '4'): 1}
-        #registros = {} 
-        registros_ordenados = {}
-
-        for hora in horas:
-            registros_ordenados[hora] = {puesto: 0 for puesto in puestos} 
+        # Diccionario de registros ordenados por hora y puesto 
+        registros_ordenados = {hora: {puesto: 0 for puesto in puestos} for hora in horas} 
 
         for registro in registros_linea_hoy:
             puesto = registro.puesto
@@ -32,50 +37,27 @@ class lineasController(http.Controller):
             if puesto and timestamp:
                 hora = timestamp.strftime('%H:00')
                 if hora in registros_ordenados:
-            # Actualiza el contador para el puesto y hora específicos
-                    registros_ordenados[hora][puesto] += 1
-                # clave =(hora,puesto)
-                # if clave in registros:
-                #     registros[clave] += 1
-                # else:
-                #     registros[clave] = 1
-
-        # # registros ordenados por hora {'07:00': {'1': 0, '2': 0, '3': 1, '4': 0, '5': 0}, '08:00': {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0}, ...}
-        # registros_ordenados =  {}
-
-        # for hora in horas:
-        #     registros_ordenados[hora] = {puesto: 0 for puesto in puestos} 
-        
-        # # Llenar registros ordenados con los valores del diccionario 'registros'
-        # for (hora, puesto), count in registros.items():
-        #     if hora in registros_ordenados:
-        #         registros_ordenados[hora][puesto] = count
-         
-        ######### OBJETIVOS ########
-        inicio_semana = hoy - timedelta(days=hoy.weekday())  # Lunes de la semana actual
-        fin_semana = inicio_semana + timedelta(days=4) # Viernes de la semana actual
-
-        # objetivos(49, 50, 51, 52, 53) -> son los id de los registros de la tabla objetivos
-        objetivos_records = request.env['objetivos'].sudo().search([
-            ('timestamp', '>=', inicio_semana),
-            ('timestamp', '<=', fin_semana)
-        ])
-
-        dias_orden = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes']
-        objetivos = {dia: {'objetivos': [], 'cantidad' : 0} for dia in dias_orden}
+                    # Actualiza el contador para el puesto y hora específicos
+                    registros_ordenados[hora][puesto] += 1 
 
         # Suma de todos los registros del dia : 4
-        total_registros = sum(
-                    registros_ordenados[hora][puesto] for hora in registros_ordenados for puesto in puestos
-                )
+        total_registros = sum(registros_ordenados[hora][puesto] for hora in registros_ordenados for puesto in puestos)
+
+        ######### Gestionar objetivos ######## 
+
+        # Obtener los registros de la tabla 'objetivos' para la semana actual
+        objetivos_records = request.env['objetivos'].sudo().search([('timestamp', '>=', inicio_semana), ('timestamp', '<=', fin_semana)]) 
         
+        # Diccionario de objetivos por día
+        objetivos = {dia: {'objetivos': [], 'cantidad' : 0} for dia in dias_orden}
+
         for obj in objetivos_records:
             if obj.dia in objetivos:
                 objetivos[obj.dia]['objetivos'].append(obj.objetivo) 
                 if obj.dia.lower() == dia_semana:
-                    # Solo actualizar el objetivo del día actual
+                    # Actualizar la cantidad en el objetivo del día actual
                     obj.write({'cantidad': total_registros})
                 objetivos[obj.dia]['cantidad'] += obj.cantidad
-        
-        print(registros_ordenados)
+         
+        # Renderizar la vista QWeb con los datos obtenidos 
         return request.render('linea.linea_qweb_view',{'docs': registros_linea_hoy, 'registros' : registros_ordenados, 'puestos' : puestos, 'horas': horas, 'hoy': hoy, 'dia_semana': dia_semana, 'hora_actual': hora_actual, 'objetivos': objetivos})
